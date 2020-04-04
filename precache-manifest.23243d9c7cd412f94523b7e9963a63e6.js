45319c24bcd4fd88e80cb10ddf8109f956685686
fahredof <turik20121998@gmail.com>
2020-04-05 01:16:22 +0300

    Updates

---
+++ precache-manifest.23243d9c7cd412f94523b7e9963a63e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29bdaacc29057410142fdbb668eb2d76",
+    "url": "/rocket-launch/index.html"
+  },
+  {
+    "revision": "6f0dd41f4c90939e7ac2",
+    "url": "/rocket-launch/static/css/main.c8e59219.chunk.css"
+  },
+  {
+    "revision": "8dc546c464a8e4cbc831",
+    "url": "/rocket-launch/static/js/2.fd01bc30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rocket-launch/static/js/2.fd01bc30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0dd41f4c90939e7ac2",
+    "url": "/rocket-launch/static/js/main.bb8fe094.chunk.js"
+  },
+  {
+    "revision": "240c43e3ebe54f1392dc",
+    "url": "/rocket-launch/static/js/runtime-main.154811cc.js"
+  },
+  {
+    "revision": "6f6870a949e89820b02af28fac149d9e",
+    "url": "/rocket-launch/static/media/spinner.6f6870a9.svg"
+  }
+]);
